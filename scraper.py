04c2141cbc1a89d4494e0468885d223fceb32ce8
Brandon Lo <blo6@uci.edu>
2026-02-06 05:19:40 -0800

    Implemented URL filtering and link extraction v1

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import re
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
+from bs4 import BeautifulSoup
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -15,7 +16,18 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    if resp.status != 200:
+        return []
+    else:
+        soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
+        hyperlinks = set()
+        all_links = soup.find_all('a')
+        for value in all_links:
+            if value.get('href'):
+                href = value.get('href')
+                full_url = urljoin(url, href)
+                hyperlinks.add(full_url)
+    return list(hyperlinks)
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
