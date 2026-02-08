158a3d981a0ba1459117b0c23977c72262ca55b4
erickgomez <egomez105@student.cccd.edu>
2026-02-07 16:22:21 -0800

    adjust syntax error

--- scraper.py
+++ scraper.py
@@ -61,8 +61,8 @@ def is_valid(url):
         if re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico|png|tiff?|mid|mp2|mp3|mp4|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
             + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
-            + r"|epub|dll|cnf|tgz|sha1|thmx|mso|arff|rtf|jar|csv|rm|smil|wmv|swf|wma|zip|rar|gz)$"
-            + r".*\.(...|sas|odc|txt)$", 
+            + r"|epub|dll|cnf|tgz|sha1|thmx|mso|arff|rtf|jar|csv|rm|smil|wmv|swf|wma|zip|rar|gz"
+            + r"|py|sas|odc|txt)$", 
             parsed.path.lower()):
             return False
         
